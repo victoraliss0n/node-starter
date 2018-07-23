5477fb204ede7c2b7a0eb967218975eee06b699d
victoraliss0n <victoralissont5@gmail.com>
2018-07-23 17:01:00 -0300

    Add category modeling with Sequelize

---
+++ models/index.js
@@ -0,0 +1,23 @@
+
+const json = {
+    username: 'root',
+    password: null,
+    database: 'dbcategories',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  }
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(json)
+
+const models = {}
+const fs = require('fs')
+const path = require('path')
+fs.readdirSync(__dirname)
+  .filter(file => file!=='index.js')
+  .forEach(file => {
+      const model = sequelize.import(path.join(__dirname, file))
+      models[model.name] = model
+  })
+
+
+module.exports = { sequelize, models }
