279bf4dd41eb8b50371444f63cf5d66f17eff54f
victoraliss0n <victoralissont5@gmail.com>
2018-07-24 04:06:35 -0300

    Update routes

--- routes/category.js
+++ routes/category.js
@@ -6,9 +6,9 @@ const indexModel = require('../models/index')
 
 router.get('/categories', categoryController.all.bind(null, indexModel.models))
 router.post('/categories', categoryController.create.bind(null, indexModel.models))
-router.delete('/deleteAll', categoryController.deleteAll.bind(null, indexModel.models))
-router.delete('/delete/:id', categoryController.destroyOne.bind(null, indexModel.models))
-router.get('/category/edit/:id', categoryController.editForm.bind(null, indexModel.models))
-router.put('/category/edit/:id', categoryController.editOne.bind(null, indexModel.models))
+router.delete('/categories/all', categoryController.deleteAll.bind(null, indexModel.models))
+router.delete('/categories/:id', categoryController.destroyOne.bind(null, indexModel.models))
+router.get('/categories/edit/:id', categoryController.editForm.bind(null, indexModel.models))
+router.put('/categories/edit/:id', categoryController.editOne.bind(null, indexModel.models))
 
 module.exports = router
