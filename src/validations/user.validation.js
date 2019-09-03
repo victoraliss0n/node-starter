e96e4e0e981d199a3f08b8f1ab76077e636b2133
Victor Alisson <victoralissont5@gmail.com>
2019-09-03 03:49:04 -0300

    Add yup with validation

---
+++ src/validations/user.validation.js
@@ -0,0 +1,17 @@
+import * as Yup from 'yup'
+export const validateUpdate = body => {
+  const schema = Yup.object().shape({
+    name: Yup.string(),
+    email: Yup.string().email(),
+    oldPassword: Yup.string().min(6),
+    password: Yup.string()
+      .min(6)
+      .when('oldPassword', (oldPassword, field) =>
+        oldPassword ? field.required() : field
+      ),
+    confirmPassword: Yup.string().when('password', (password, field) =>
+      password ? field.required().oneOf([Yup.ref('password')]) : field
+    ),
+  })
+  return schema.isValid(body)
+}
