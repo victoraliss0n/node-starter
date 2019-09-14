dc5618750ee106104a3b88a59a3a6d2d4fc56042
Victor Alisson <victoralissont5@gmail.com>
2019-09-14 16:51:53 -0300

    Fix: invalid name method

--- src/app/queues/Queue.js
+++ src/app/queues/Queue.js
@@ -28,7 +28,7 @@ export default class Queue {
   processQueue() {
     jobs.forEach(job => {
       const { bee, handle } = this.queues[job.key]
-      bee.on('failed', this.handleFailed).process(handle)
+      bee.on('failed', this.handleFailure).process(handle)
     })
   }
 
